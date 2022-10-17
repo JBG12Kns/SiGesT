1d6875a8e82a36a77776e50b0f1331770f9166de
JBG12Kns <joaomsdiogo@gmail.com>
2022-10-17 12:51:13 +0100

    Add project files.

---
+++ NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+internal static class NativeMethods
+{
+    [DllImport("user32.dll")]
+    internal static extern int MessageBoxA(
+        IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    internal static extern int MessageBoxW(
+        IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    internal static extern int MessageBox(
+        IntPtr hWnd, string lpText, string lpCaption, uint uType);
+}
---
+++ SiGest.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace SiGesT
+{
+    partial class frmSiGest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSiGest));
+            this.pnlBase = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnEmparceira = new System.Windows.Forms.Button();
+            this.btnClassifica = new System.Windows.Forms.Button();
+            this.btnResultados = new System.Windows.Forms.Button();
+            this.btnExtras = new System.Windows.Forms.Button();
+            this.btnInscreveEdita = new System.Windows.Forms.Button();
+            this.btnInscreveEquipa = new System.Windows.Forms.Button();
+            this.btnInscreveInd = new System.Windows.Forms.Button();
+            this.btnIniciarTorneio = new System.Windows.Forms.Button();
+            this.lblSiGesTor = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblNomeTor = new System.Windows.Forms.Label();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pnlBase.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBase
+            // 
+            this.pnlBase.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pnlBase.Controls.Add(this.button2);
+            this.pnlBase.Controls.Add(this.button1);
+            this.pnlBase.Controls.Add(this.pictureBox1);
+            this.pnlBase.Controls.Add(this.btnEmparceira);
+            this.pnlBase.Controls.Add(this.btnClassifica);
+            this.pnlBase.Controls.Add(this.btnResultados);
+            this.pnlBase.Controls.Add(this.btnExtras);
+            this.pnlBase.Controls.Add(this.btnInscreveEdita);
+            this.pnlBase.Controls.Add(this.btnInscreveEquipa);
+            this.pnlBase.Controls.Add(this.btnInscreveInd);
+            this.pnlBase.Controls.Add(this.btnIniciarTorneio);
+            this.pnlBase.Controls.Add(this.lblSiGesTor);
+            this.pnlBase.Location = new System.Drawing.Point(0, 0);
+            this.pnlBase.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pnlBase.Name = "pnlBase";
+            this.pnlBase.Size = new System.Drawing.Size(229, 1259);
+            this.pnlBase.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(161, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnEmparceira
+            // 
+            this.btnEmparceira.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnEmparceira.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmparceira.Location = new System.Drawing.Point(14, 454);
+            this.btnEmparceira.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnEmparceira.Name = "btnEmparceira";
+            this.btnEmparceira.Size = new System.Drawing.Size(200, 35);
+            this.btnEmparceira.TabIndex = 8;
+            this.btnEmparceira.Text = "Emparceiramentos";
+            this.btnEmparceira.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmparceira.UseVisualStyleBackColor = false;
+            // 
+            // btnClassifica
+            // 
+            this.btnClassifica.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnClassifica.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClassifica.Location = new System.Drawing.Point(14, 418);
+            this.btnClassifica.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnClassifica.Name = "btnClassifica";
+            this.btnClassifica.Size = new System.Drawing.Size(200, 35);
+            this.btnClassifica.TabIndex = 7;
+            this.btnClassifica.Text = "Classificações";
+            this.btnClassifica.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClassifica.UseVisualStyleBackColor = false;
+            // 
+            // btnResultados
+            // 
+            this.btnResultados.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnResultados.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultados.Location = new System.Drawing.Point(14, 383);
+            this.btnResultados.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnResultados.Name = "btnResultados";
+            this.btnResultados.Size = new System.Drawing.Size(200, 35);
+            this.btnResultados.TabIndex = 6;
+            this.btnResultados.Text = "Inserir Resultados";
+            this.btnResultados.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnResultados.UseVisualStyleBackColor = false;
+            // 
+            // btnExtras
+            // 
+            this.btnExtras.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnExtras.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtras.Location = new System.Drawing.Point(12, 140);
+            this.btnExtras.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnExtras.Name = "btnExtras";
+            this.btnExtras.Size = new System.Drawing.Size(200, 35);
+            this.btnExtras.TabIndex = 5;
+            this.btnExtras.Text = "Extras";
+            this.btnExtras.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExtras.UseVisualStyleBackColor = false;
+            // 
+            // btnInscreveEdita
+            // 
+            this.btnInscreveEdita.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnInscreveEdita.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscreveEdita.Location = new System.Drawing.Point(12, 224);
+            this.btnInscreveEdita.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnInscreveEdita.Name = "btnInscreveEdita";
+            this.btnInscreveEdita.Size = new System.Drawing.Size(200, 35);
+            this.btnInscreveEdita.TabIndex = 4;
+            this.btnInscreveEdita.Text = "Novo Editar";
+            this.btnInscreveEdita.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInscreveEdita.UseVisualStyleBackColor = false;
+            // 
+            // btnInscreveEquipa
+            // 
+            this.btnInscreveEquipa.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnInscreveEquipa.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscreveEquipa.Location = new System.Drawing.Point(12, 321);
+            this.btnInscreveEquipa.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnInscreveEquipa.Name = "btnInscreveEquipa";
+            this.btnInscreveEquipa.Size = new System.Drawing.Size(200, 35);
+            this.btnInscreveEquipa.TabIndex = 3;
+            this.btnInscreveEquipa.Text = "Inscrição Equipa";
+            this.btnInscreveEquipa.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInscreveEquipa.UseVisualStyleBackColor = false;
+            // 
+            // btnInscreveInd
+            // 
+            this.btnInscreveInd.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnInscreveInd.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscreveInd.Location = new System.Drawing.Point(12, 286);
+            this.btnInscreveInd.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnInscreveInd.Name = "btnInscreveInd";
+            this.btnInscreveInd.Size = new System.Drawing.Size(200, 35);
+            this.btnInscreveInd.TabIndex = 2;
+            this.btnInscreveInd.Text = "Inscrição Individual";
+            this.btnInscreveInd.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInscreveInd.UseVisualStyleBackColor = false;
+            // 
+            // btnIniciarTorneio
+            // 
+            this.btnIniciarTorneio.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnIniciarTorneio.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarTorneio.Location = new System.Drawing.Point(12, 93);
+            this.btnIniciarTorneio.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnIniciarTorneio.Name = "btnIniciarTorneio";
+            this.btnIniciarTorneio.Size = new System.Drawing.Size(200, 35);
+            this.btnIniciarTorneio.TabIndex = 1;
+            this.btnIniciarTorneio.Text = "Iniciar Torneio";
+            this.btnIniciarTorneio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciarTorneio.UseVisualStyleBackColor = false;
+            // 
+            // lblSiGesTor
+            // 
+            this.lblSiGesTor.AutoSize = true;
+            this.lblSiGesTor.Font = new System.Drawing.Font("Myriad Pro Cond", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSiGesTor.Location = new System.Drawing.Point(5, 6);
+            this.lblSiGesTor.Name = "lblSiGesTor";
+            this.lblSiGesTor.Size = new System.Drawing.Size(109, 39);
+            this.lblSiGesTor.TabIndex = 1;
+            this.lblSiGesTor.Text = "SiGesTor";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.lblNomeTor);
+            this.panel2.Controls.Add(this.lblInicio);
+            this.panel2.Location = new System.Drawing.Point(247, 17);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(666, 893);
+            this.panel2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Myriad Pro Cond", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(149, 73);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(304, 31);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblNomeTor
+            // 
+            this.lblNomeTor.AutoSize = true;
+            this.lblNomeTor.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeTor.Location = new System.Drawing.Point(17, 76);
+            this.lblNomeTor.Name = "lblNomeTor";
+            this.lblNomeTor.Size = new System.Drawing.Size(55, 25);
+            this.lblNomeTor.TabIndex = 10;
+            this.lblNomeTor.Text = "Nome";
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Font = new System.Drawing.Font("Myriad Pro Cond", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.Location = new System.Drawing.Point(247, 5);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(101, 39);
+            this.lblInicio.TabIndex = 9;
+            this.lblInicio.Text = "Torneio";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 190);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Update Lista AdlG";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.Font = new System.Drawing.Font("Myriad Pro Cond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(14, 529);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 35);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exportar Resultados";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // frmSiGest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1661, 806);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlBase);
+            this.Font = new System.Drawing.Font("Myriad Pro Cond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "frmSiGest";
+            this.Text = "SiGesTor";
+            this.Load += new System.EventHandler(this.SiGest_Load);
+            this.pnlBase.ResumeLayout(false);
+            this.pnlBase.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBase;
+        private System.Windows.Forms.Button btnEmparceira;
+        private System.Windows.Forms.Button btnClassifica;
+        private System.Windows.Forms.Button btnResultados;
+        private System.Windows.Forms.Button btnExtras;
+        private System.Windows.Forms.Button btnInscreveEdita;
+        private System.Windows.Forms.Button btnInscreveEquipa;
+        private System.Windows.Forms.Button btnInscreveInd;
+        private System.Windows.Forms.Button btnIniciarTorneio;
+        private System.Windows.Forms.Label lblSiGesTor;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblNomeTor;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
